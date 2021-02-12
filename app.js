9cfe3504213d25542bfb8e78e6b11fbddfe7cd3e
Idzel Jaimes <irjaimes@outlook.com>
2021-02-11 17:40:48 -0800

    used inquire to prompt questions and answers. added ability to add multiple projects

--- app.js
+++ app.js
@@ -35,12 +35,90 @@ const inquirer = require("inquirer")
 //   console.log('Portfolio complete! Check out index.html to see output!');
 // });
 
-inquirer
-.prompt([
-  {
-    type: 'input',
-    name: 'name',
-    message: 'What is your name?'
+// function that uses inquirer's prompt method
+const promptUser = () => {
+  // this is used as a promise
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your Github Username'
+    },
+    {
+      type: 'input',
+      name: 'about',
+      message: 'Provide some information about yourself:'
+    }
+  ]);
+};
+// invoke the promptUser function to return inquirer.prompt method as promise. appernd the  .then() mothod to resolve promise by returning answers
+//promptUser().then(answers => console.log(answers));
+
+const promptProject = portfolioData => { // use console logs to devide prompts
+
+  // If there's no 'projects' array property, create one
+  if (!portfolioData.projects) {
+    portfolioData.projects = [];
   }
-])
-.then(answers => console.log(answers));
+
+  console.log(`
+  ============= 
+  Add a New Project
+  =============
+  `);
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is the name of your project?'
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a description of the project (Required)'
+    },
+    {
+      type: 'checkbox',
+      name: 'languages',
+      message: 'What did you build this project with? (Check all that apply)',
+      choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+    },
+    {
+      type: 'input',
+      name: 'link',
+      message: 'Enter the GitHub link to your project. (Required)'
+    },
+    {
+      type: 'confirm',
+      name: 'feature',
+      message: 'Would you like to feature this project?',
+      default: false
+    },
+    {
+      type: 'confirm',
+      name: 'confirmAddProject',
+      message: 'Would you like to enter another project?',
+      default: false
+    }
+  ])
+    .then(projectData => {
+      portfolioData.projects.push(projectData);
+      if (projectData.confirmAddProject) {
+        return promptProject(portfolioData);
+      } else {
+        return portfolioData;
+      }
+    })
+
+};
+
+promptUser()
+  .then(promptProject)
+  .then(portfolioData => {
+    console.log(portfolioData);
+  });
